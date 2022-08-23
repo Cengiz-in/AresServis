dda5cf543c7613422a9929f2c7074763a63e1dc7
cihangircengiz <nowdyle@gmail.com>
2022-08-23 11:43:25 +0300

    edit için düzenleme

--- frontend/src/app/redux/actions/VehicleActions.js
+++ frontend/src/app/redux/actions/VehicleActions.js
@@ -3,23 +3,20 @@ import axios from 'axios.js';
 export const GET_VEHICLE = 'GET_VEHICLE';
 export const PUT_VEHICLE = 'PUT_VEHICLE';
 
-export const getVehicles = (id) => (dispatch) => {
-    axios.get(`/vehicle/vehicles`).then((res) => {
-   console.log('res');
-   console.log(res);
-        dispatch({
-            type:GET_VEHICLE,
-            payload: res.data.result,
-        });
+export const getVehicles = () => (dispatch) => {
+  axios.get(`/vehicle/vehicles`).then((res) => {
+    dispatch({
+      type: GET_VEHICLE,
+      payload: res.data.result,
     });
-}
+  });
+};
 
-
-export const putVehicles = (vehicleId) => (dispatch) => {
-    axios.put(`/Vehicle/update/${vehicleId}`).then((res)=>{
-        dispatch({
-            type: PUT_VEHICLE,
-            payload: res.data.result,
-        });
+export const putVehicles = (vehicleId,payload) => (dispatch) => {
+  axios.put(`/Vehicle/update/${vehicleId}`,payload).then((res) => {
+    dispatch({
+      type: PUT_VEHICLE,
+      payload: res.data.result,
     });
+  });
 };
--- frontend/src/app/views/vehicle/Vehicle.jsx
+++ frontend/src/app/views/vehicle/Vehicle.jsx
@@ -1,83 +1,75 @@
 import * as React from 'react';
-import { useEffect, useState } from "react";
-import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import FormDialog from "app/components/FormDialog";
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import EditIcon from '@mui/icons-material/Edit';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import {
-  DataGrid,
-} from '@mui/x-data-grid';
-import { useParams } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { DataGrid } from '@mui/x-data-grid';
+import { useDispatch, useSelector } from 'react-redux';
 import { getVehicles, putVehicles } from 'app/redux/actions/VehicleActions';
-
-
-
 
 export default function Vehicle() {
   const dispatch = useDispatch();
-  const { id } = useParams();
-  const {vehicles} = useSelector((state) => state.vehicle);
+  const { vehicles } = useSelector((state) => state.vehicle);
   const [openEdit, setOpenEdit] = useState(false);
+  const [editVehicleItem, setEditVehicleItem] = useState(null);
 
   const handleEditOpen = (event, cellValues) => {
-    //console.log(cellValues)
+    setEditVehicleItem(cellValues.row);
     setOpenEdit(true);
   };
 
   const handleEditClose = () => {
+    setEditVehicleItem(null);
     setOpenEdit(false);
-  }
-
-  const handleClick = (event, cellValues) => {
-    console.log(cellValues.row);
-  };
-  const handleCellClick = (param, event) => {
-    event.stopPropagation();
-  };
-  
-  const handleRowClick = (param, event) => {
-    event.stopPropagation();
   };
 
-  useEffect (() => {
+  useEffect(() => {
     try {
-     dispatch(getVehicles(id));
+      dispatch(getVehicles());
     } catch (e) {
       alert.error(e);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const submitVehicleForm =  (id) => {
-    console.log(id)
-     dispatch(putVehicles(id));
-  
+  const submitVehicleForm = () => {
+    dispatch(putVehicles(editVehicleItem));
   };
-  
+
   const columns = [
-    { field: "id", headerName: '#', type: 'number', editable: false },
-    { field: "plateNumber", headerName: 'Plaka', type: 'string', editable: true },
-    { field: "vehicleDriver", headerName: 'Sürücü bilgileri', type: 'string', width: 200, editable: false},
+    { field: 'id', headerName: '#', type: 'number', editable: false },
     {
-      field: "device",
-      headerName: 'cihaz',
+      field: 'plateNumber',
+      headerName: 'Plaka',
+      type: 'string',
+      editable: true,
+    },
+    {
+      field: 'vehicleDriver',
+      headerName: 'Sürücü bilgileri',
+      type: 'string',
+      width: 200,
+      editable: false,
+    },
+    {
+      field: 'device',
+      headerName: 'Cihaz',
       type: 'string',
       width: 220,
       editable: true,
-    },  
+      valueGetter: (params) => {
+        return params.row.device.name;
+      },
+    },
     {
-      field: "Actions",
+      field: 'Actions',
       headerName: 'İşlemler',
       type: 'string',
       width: 220,
-      field: "Actions",
       renderCell: (cellValues) => {
         return (
           <Button
@@ -91,16 +83,11 @@ export default function Vehicle() {
             Düzenle
           </Button>
         );
-      }
-      
-    },  
-   
-  ]
-  
-  
+      },
+    },
+  ];
 
   return (
-    
     <Box
       sx={{
         height: 500,
@@ -112,16 +99,9 @@ export default function Vehicle() {
           color: 'text.primary',
         },
       }}
-      
-
-      
     >
-      
-      <DataGrid
-        rows={vehicles}
-        columns={columns}
-      />  
-        
+      <DataGrid rows={vehicles} columns={columns} />
+
       <Dialog open={openEdit} onClose={handleEditClose}>
         <DialogTitle>Araç plaka değiştir</DialogTitle>
         <DialogContent>
@@ -134,14 +114,13 @@ export default function Vehicle() {
             type="text"
             fullWidth
             variant="standard"
-            
           />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleEditClose}>İptal</Button>
           <Button onClick={submitVehicleForm}>Güncelle</Button>
         </DialogActions>
-      </Dialog>  
+      </Dialog>
     </Box>
   );
 }
