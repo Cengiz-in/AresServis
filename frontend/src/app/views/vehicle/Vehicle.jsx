bc1e20fd4cb4239b9966ea6b7725e38be1d5121b
cihangircengiz <nowdyle@gmail.com>
2022-08-24 12:25:39 +0300

    araç düzenlemeri eklendi

--- frontend/src/app/views/vehicle/Vehicle.jsx
+++ frontend/src/app/views/vehicle/Vehicle.jsx
@@ -1,38 +1,61 @@
-import * as React from "react";
-import { useEffect, useState } from "react";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import DialogContent from "@mui/material/DialogContent";
-import DialogTitle from "@mui/material/DialogTitle";
-import { DataGrid } from "@mui/x-data-grid";
-import { useDispatch, useSelector } from "react-redux";
-import { getVehicles, putVehicles,resetVehicleErrors } from "app/redux/actions/VehicleActions";
-import { Formik } from "formik";
-import * as Yup from "yup";
-
-
-const initialValues = {
-  plateNumber: "",
-  device: 0,
-};
-// form field validation schema
-const validationSchema = Yup.object().shape({
-  plateNumber: Yup.string().required("Plaka boş geçilemez!"),
-  device: Yup.string().required("device alanı boş geçilemez!"),
-});
+import * as React from 'react';
+import { useEffect, useState } from 'react';
+import Select from '@mui/material/Select';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { DataGrid } from '@mui/x-data-grid';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  getVehicles,
+  putVehicles,
+  resetVehicleErrors,
+} from 'app/redux/actions/VehicleActions';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
 
 export default function Vehicle() {
   const dispatch = useDispatch();
-  const { vehicles, vehicleSucceded, vehicleErrors } = useSelector((state) => state.vehicle);
+  const { vehicles, vehicleSucceded, vehicleErrors } = useSelector(
+    (state) => state.vehicle
+  );
   const [openEdit, setOpenEdit] = useState(false);
   const [editVehicleId, setEditVehicleId] = useState(null);
-  //const { enqueueSnackbar, Snackbar } = useSnackbar();
+  const [vehicleInformation, setVehicleInformation] = useState({
+    plateNumber: '',
+    deviceId: 0,
+  });
+
+  const deviceList = [
+    { id: 1, value: 'ARES01' },
+    { id: 3, value: 'ARES02' },
+    { id: 4, value: 'ARES03' },
+    { id: 5, value: 'ARES04' },
+    { id: 6, value: 'ARES05' },
+    { id: 7, value: 'ARES06' },
+    { id: 8, value: 'ARES07' },
+    { id: 10, value: 'ARES08' },
+    { id: 11, value: 'ARES09' },
+    { id: 13, value: 'ARES10' },
+    { id: 14, value: 'ARES' },
+  ];
+
+  // form field validation schema
+  const validationSchema = Yup.object().shape({
+    plateNumber: Yup.string().required('Plaka boş geçilemez!'),
+    deviceId: Yup.string().required('Device alanı boş geçilemez!'),
+  });
 
   const handleEditOpen = (event, cellValues) => {
-    
+    setVehicleInformation({
+      plateNumber: cellValues.row.plateNumber,
+      deviceId: cellValues.row.device.id,
+    });
     setEditVehicleId(cellValues.row.id);
     setOpenEdit(true);
   };
@@ -47,21 +70,20 @@ export default function Vehicle() {
     } catch (e) {
       alert.error(e);
     }
-    
   }, []);
-  
+
+
   useEffect(() => {
     if (vehicleErrors != null) {
-      const variant = "error";
-     // enqueueSnackbar(vehicleErrors, { variant });
+      const variant = 'error';
+      // enqueueSnackbar(vehicleErrors, { variant });
       dispatch(resetVehicleErrors());
     }
   }, [vehicleErrors, dispatch]);
 
-
- useEffect(() => {
+  useEffect(() => {
     if (vehicleSucceded != null) {
-      const variant = "success";
+      const variant = 'success';
       //enqueueSnackbar("Başarılı", { variant });
       dispatch(resetVehicleErrors());
       handleEditClose();
@@ -69,42 +91,42 @@ export default function Vehicle() {
   }, [vehicleSucceded, dispatch]);
 
   const submitVehicleForm = async (values) => {
-    console.log(values)
     dispatch(putVehicles(editVehicleId, values));
+    handleEditClose();
+    dispatch(getVehicles());
   };
 
   function getDevice(params) {
-    return `${params.row.id || ''}`;
+    return `${params.value.name || ''}`;
   }
 
-
   const columns = [
-    { field: "id", headerName: "#", type: "number", editable: false },
+    { field: 'id', headerName: '#', type: 'number', editable: false },
     {
-      field: "plateNumber",
-      headerName: "Plaka",
-      type: "string",
+      field: 'plateNumber',
+      headerName: 'Plaka',
+      type: 'string',
       editable: true,
     },
     {
-      field: "vehicleDriver",
-      headerName: "Sürücü bilgileri",
-      type: "string",
+      field: 'vehicleDriver',
+      headerName: 'Sürücü bilgileri',
+      type: 'string',
       width: 200,
       editable: false,
     },
     {
-      field: "device",
-      headerName: "Cihaz",
-      type: "string",
+      field: 'device',
+      headerName: 'Cihaz',
+      type: 'string',
       width: 220,
       editable: true,
-      valueGetter:getDevice
+      valueGetter: getDevice,
     },
     {
-      field: "Actions",
-      headerName: "İşlemler",
-      type: "string",
+      field: 'Actions',
+      headerName: 'İşlemler',
+      type: 'string',
       width: 220,
       renderCell: (cellValues) => {
         return (
@@ -127,12 +149,12 @@ export default function Vehicle() {
     <Box
       sx={{
         height: 500,
-        width: "100%",
-        "& .actions": {
-          color: "text.secondary",
+        width: '100%',
+        '& .actions': {
+          color: 'text.secondary',
         },
-        "& .textPrimary": {
-          color: "text.primary",
+        '& .textPrimary': {
+          color: 'text.primary',
         },
       }}
     >
@@ -143,7 +165,7 @@ export default function Vehicle() {
         <DialogContent>
           <Formik
             onSubmit={submitVehicleForm}
-            initialValues={initialValues}
+            initialValues={vehicleInformation}
             validationSchema={validationSchema}
           >
             {({
@@ -153,6 +175,8 @@ export default function Vehicle() {
               handleChange,
               handleBlur,
               handleSubmit,
+              isValid,
+              dirty,
             }) => (
               <form onSubmit={handleSubmit}>
                 <TextField
@@ -169,23 +193,31 @@ export default function Vehicle() {
                   error={Boolean(errors.plateNumber && touched.plateNumber)}
                   sx={{ mb: 3 }}
                 />
-                  <TextField
+                <Select
                   fullWidth
-                  size="small"
-                  type="text"
-                  name="device"
-                  label="Device"
+                  value={values.deviceId}
+                  name="deviceId"
                   variant="outlined"
-                  onBlur={handleBlur}
-                  value={values.device}
                   onChange={handleChange}
-                  helperText={touched.deviceId && errors.deviceId}
+                  onBlur={handleBlur}
+                  autoWidth
+                  label="Device"
                   error={Boolean(errors.deviceId && touched.deviceId)}
                   sx={{ mb: 3 }}
-                />
+                >
+                  {deviceList.map((element, idx) => {
+                    return (
+                      <MenuItem value={element.id} key={idx}>
+                        {element.value}
+                      </MenuItem>
+                    );
+                  })}
+                </Select>
                 <DialogActions>
                   <Button onClick={handleEditClose}>Kapat</Button>
-                  <Button type="submit">Güncelle</Button>
+                  <Button type="submit" disabled={!isValid || !dirty}>
+                    Güncelle
+                  </Button>
                 </DialogActions>
               </form>
             )}
--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const { REACT_APP_API_HOST } = process.env;
 
 const axiosInstance = axios.create({
-  baseURL: 'https://localhost:7079/api'
+  baseURL: `${REACT_APP_API_HOST}/api`
 });
 
 axiosInstance.interceptors.response.use(
