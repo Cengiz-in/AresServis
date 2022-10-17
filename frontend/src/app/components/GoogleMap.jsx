d3136c9742ec1930516766a75386203a214f1aae
cihangircengiz <nowdyle@gmail.com>
2022-10-17 12:20:07 +0300

    geocode added

--- frontend/src/app/components/GoogleMap.jsx
+++ frontend/src/app/components/GoogleMap.jsx
@@ -1,90 +1,71 @@
-import {
-  Grid,
-  Icon,
-  Typography,
-} from '@mui/material';
-import React from "react";
-import { useState } from "react";
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
-import { Polyline } from '@react-google-maps/api';
 import useWindowDimensions from 'app/hooks/useWindowDimensions';
 import { sidenavCompactWidth } from 'app/utils/constant';
 
-
-
-
-
-const BusIcon = ({ text, isActive }) => {
-  return (
-    <Grid container direction="row" alignItems="center" justifyContent="center">
-      <Grid item>
-        <Typography variant="subtitle1" color={(isActive) ? "secondary" : "error"}>
-          {text}
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Icon className="icon" color={(isActive) ? "secondary" : "error"}>
-        my_location
-        </Icon>
-      </Grid>
-    </Grid>
-  );
-};
-
-
-
 const GoogleMap = ({ history }) => {
   const { height, width } = useWindowDimensions();
+  const [map, setMap] = useState(null);
+  const [maps, setMaps] = useState(null);
   const defaultProps = {
     center: {
       lat: 38.423733,
-      lng: 27.142826
+      lng: 27.142826,
     },
-    zoom: 11
+    zoom: 11,
   };
 
+  const getMaps = (map,maps) => {
+    setMap(map);
+    setMaps(maps);
+  }
 
-  const onLoad = polyline => {
-    console.log('polyline: ', polyline)
+  useEffect(() => {
+    if(history){
+    const locations = history.map((item) => {
+      return {
+        lat: item.latitude,
+        lng: item.longitude
+      };
+    });
+    renderPolylines(map,maps,locations);
+  }
+  }, [history]);
+
+  const renderPolylines = (map, maps,markers) => {
+    // /** Example of rendering geodesic polyline */
+    let geodesicPolyline = new maps.Polyline({
+      path: markers,
+      geodesic: true,
+      strokeColor: '#00a1e1',
+      strokeOpacity: 1.0,
+      strokeWeight: 4,
+    });
+    geodesicPolyline.setMap(map);
+
+    fitBounds(map, maps,markers);
   };
 
-  
-  
+  const fitBounds = (map, maps,markers) => {
+    var bounds = new maps.LatLngBounds();
+    for (let marker of markers) {
+      bounds.extend(new maps.LatLng(marker.lat, marker.lng));
+    }
+    map.fitBounds(bounds);
+  };
 
   return (
-    <div style={{ height: height - sidenavCompactWidth, width: "100%" }}>
+    <div style={{ height: height - sidenavCompactWidth, width: '100%' }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyAXy8dgvhceCnZtQgSVcKK1uj23-0fD_TY" }}
+        bootstrapURLKeys={{ key: 'AIzaSyAXy8dgvhceCnZtQgSVcKK1uj23-0fD_TY' }}
         resetBoundsOnResize={true}
         defaultCenter={defaultProps.center}
         defaultZoom={defaultProps.zoom}
-      >
-        {history &&
-          history.map((history, i) => (
-            <BusIcon key={i} lat={history.latitude} lng={history.longitude} />
-          ))}
-
-        {history &&
-          history.map((history, i) => (
-            <Polyline
-              key={i}
-              onLoad={onLoad}
-              path={{
-                lat: history.latitude,
-                lng: history.longitude,
-              }}
-              options={{
-                strokeColor: "#000000",
-                strokeOpacity: 0.8,
-                strokeWeight: 2,
-                fillColor: "#000000",
-                fillOpacity: 0.35,
-              }}
-            />
-          ))}
-      </GoogleMapReact>
+        onGoogleApiLoaded={({ map, maps }) => getMaps(map, maps)}
+      ></GoogleMapReact>
     </div>
   );
-}
+};
 
-export default GoogleMap
+export default GoogleMap;
